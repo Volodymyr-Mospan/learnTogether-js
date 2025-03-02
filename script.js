04d2f30f1610f5a3c6728babad166916514533ab
Volodymyr Mospan <mospan.volodymyr@gmail@.com>
2025-03-02 20:56:33 +0200

    adds hw4 and hw7

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 // Lesson 2 - 250209
-const tet = "ret";
-console.log(tet.slice(1), "some");
-console.log(tet.padStart(16, "U+0020"));
+
+// const tet = "ret";
+// console.log(tet.slice(1), "some");
+// console.log(tet.padStart(16, "U+0020"));
+
+// ==================================================================================
+
+// Lesson 8 - 250223
+
+const someText = String(undefined);
+console.log(someText, typeof someText, undefined, typeof undefined);
