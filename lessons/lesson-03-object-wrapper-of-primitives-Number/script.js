fbf5fd2d78142a57dd5ae5c649fd47afbc16c8ff
Volodymyr Mospan <mospan.volodymyr@gmail@.com>
2025-04-10 20:56:02 +0300

    adds lesson 3

---
+++ lessons/lesson-03-object-wrapper-of-primitives-Number/script.js
@@ -0,0 +1,135 @@
+// const x = 456
+// console.log(x + 1);
+
+// const str1 = 'Hi Ivan'
+// console.log(str1);
+// console.log(str1.length);
+
+// console.dir(new Number);
+// console.dir(new Number());
+// console.dir(new Number(11));
+// const str2 = '345'
+// // console.dir(typeof Number(str2));
+// console.dir(new Number(str2));
+// console.dir(new Number(111));
+// console.dir(new String(str2));
+// console.dir(new Boolean(str2));
+// console.dir(Object(Symbol(str2)));
+
+// const obj2 = {
+//   x: 1,
+//   test: true,
+//   age: 34,
+//   cars: {
+//     model: 'toyota',
+//     year: 2034,
+//     toyota: null
+//   }
+// }
+// console.dir(obj2);
+
+// тип Number (IEEE 754)
+
+
+// console.log(Number);
+// console.dir(Number);
+// console.dir(typeof Number);
+// console.dir(String);
+// console.dir(Boolean);
+// console.log(Math);
+
+// console.log(Number.MIN_VALUE); 
+
+// console.log(Number.MAX_VALUE);
+// console.log(-Number.MAX_VALUE);
+
+// console.log(Number.MIN_SAFE_INTEGER); // -9007199254740991 (-(2^53 - 1))
+// console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991 (2^53 - 1)
+
+// console.log(Number.POSITIVE_INFINITY); // Infinity
+// console.log(Number.NEGATIVE_INFINITY); // -Infinity
+
+// console.log(Number.MAX_VALUE + Number.MAX_VALUE); // Infinity
+// console.log(-Number.MAX_VALUE - Number.MAX_VALUE); // -Infinity
+
+// console.log(Number.MIN_VALUE/2); // 0
+// console.log(Number.MAX_VALUE * 2);
+// console.log(2 ** 1024);
+
+// // Приклади оголошення чисел
+// let x = 10; // ціле число
+// let y = 3.14; // десяткове число
+
+// console.log(0.1 + 0.2); 
+// // console.log(1 / 3); 
+
+// // Порівняння чисел з плаваючою крапкою
+// const result = 0.1 + 0.2;
+// const expected = 0.3;
+
+// const tolerance = 0.0001; // Припустима похибка
+
+// if (Math.abs(result - expected) < tolerance) {
+//     console.log('Результат в межах припустимої похибки');
+// } else {
+//     console.log('Результат за межами припустимої похибки');
+// }
+
+// Експоненціальний запис
+// let num1 = 1.23e6; // 1.23 * 10^6 = 1230000
+// let num2 = 0.00000123; // 1.23e-6
+
+// Використання розділювача _ для читабельності
+// let num3 = 1_000_000; // 1000000
+// let num4 = 1_234.56_789; // 1234.56789
+// console.log(num3, num4);
+
+// let sum = 2 + 3; // додавання
+// let difference = 5 - 2; // віднімання
+// let product = 2 * 3; // множення
+// let quotient = 6 / 2; // ділення
+// let power = 2 ** 3; // піднесення до степеня
+
+// let remainder1 = 10 % 3; // остача від ділення
+// let remainder2 = 8 % 2; // остача від ділення
+// console.log(remainder1);
+// console.log(remainder2);
+
+// let num1 = 33
+// num1 = num1 + 1
+// console.log(num1);
+
+// num1 = num1 + 4
+
+// num1 += 1
+// console.log(num1);
+// Альтернативні математичні операції
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+// ++ // increment +1
+// -- // decrement -1
+
+let num1 = 33
+// num1++
+// ++num1
+// console.log(num1);
+
+// console.log(num1 + 1);
+// console.log(num1 += 1);
+// console.log(++num1);
+console.log(num1++);
+console.log(num1);
+
+
+
+
+
+let num2 = 44
+// num2--
+// --num2
+// console.log(num2);
