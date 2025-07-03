c2e2bfa0aeacae09d7a319a434979e7aac708059
Volodymyr <109673202+Volodymyr-Mospan@users.noreply.github.com>
2025-07-03 12:47:50 +0300

    adds logic for interest-only method to hw17 dom js

--- homeWork/DOM/hw17-DOM-volodymyr-mospan/script.js
+++ homeWork/DOM/hw17-DOM-volodymyr-mospan/script.js
@@ -1,13 +1,20 @@
 "use strict";
+const form = document.querySelector(".form");
+const resetBtn = document.querySelector(".reset-btn");
 const resultDesk = document.querySelector(".result-desk");
+const resultTitle = document.querySelector(".result-title");
+const resultDescription = document.querySelector(".result-description");
 const monthlyRepayments = document.querySelector(".monthly-repayments");
 const repayOverTerm = document.querySelector(".repay-over-term");
-const form = document.querySelector("form");
 form.addEventListener("submit", onSubmit);
+resetBtn.addEventListener("click", onReset);
 function onSubmit(e) {
     e.preventDefault();
     resultDesk.classList.remove("empty");
     resultDesk.classList.add("calculated");
+    resultTitle.textContent = "Your results";
+    resultDescription.textContent =
+        "Your results are shown below based on the information you provided. To adjust the results, edit the form and click “calculate repayments” again.";
     const form = e.target;
     const amount = form.elements.namedItem("amount")
         .valueAsNumber;
@@ -16,18 +23,30 @@ function onSubmit(e) {
     const rate = form.elements.namedItem("rate")
         .valueAsNumber;
     const mortgageType = form.elements.namedItem("mortgageType").value;
-    const P = amount;
+    console.log(rate);
     const r = rate / 12 / 100;
-    const n = term * 12;
-    const M = P * ((r * (1 + r) ** n) / ((1 + r) ** n - 1));
-    monthlyRepayments.textContent = formatCurrency(M);
-    repayOverTerm.textContent = formatCurrency(M * n);
-    //   monthlyRepayments.textContent = `${String(M.toFixed(2))}`;
-    //   repayOverTerm.textContent = `${String((M * n).toFixed(2))}`;
-    console.log("Amount:", P);
-    console.log("r", r);
-    console.log("n:", n);
-    console.log("Mortgage Type:", mortgageType);
+    console.log(r);
+    const months = term * 12;
+    console.log(months);
+    if (mortgageType === "repayment") {
+        const M = amount * ((r * (1 + r) ** months) / ((1 + r) ** months - 1));
+        console.log(M);
+        monthlyRepayments.textContent = formatCurrency(M);
+        repayOverTerm.textContent = formatCurrency(M * months);
+    }
+    if (mortgageType === "interest-only") {
+        const M = amount * r;
+        monthlyRepayments.textContent = formatCurrency(M);
+        repayOverTerm.textContent = formatCurrency(M * months);
+    }
+}
+function onReset(e) {
+    form.reset();
+    resultDesk.classList.remove("calculated");
+    resultDesk.classList.add("empty");
+    resultTitle.textContent = "Results shown here";
+    resultDescription.textContent =
+        "Complete the form and click “calculate repayments” to see what your monthly repayments would be.";
 }
 function formatCurrency(repayment) {
     return repayment.toLocaleString("en-GB", {
--- homeWork/DOM/hw17-DOM-volodymyr-mospan/script.ts
+++ homeWork/DOM/hw17-DOM-volodymyr-mospan/script.ts
@@ -1,18 +1,27 @@
+const form = document.querySelector(".form") as HTMLFormElement;
+const resetBtn = document.querySelector(".reset-btn") as HTMLButtonElement;
 const resultDesk = document.querySelector(".result-desk") as HTMLDivElement;
+const resultTitle = document.querySelector(".result-title") as HTMLTitleElement;
+const resultDescription = document.querySelector(
+  ".result-description"
+) as HTMLParagraphElement;
 const monthlyRepayments = document.querySelector(
   ".monthly-repayments"
 ) as HTMLParagraphElement;
 const repayOverTerm = document.querySelector(
   ".repay-over-term"
 ) as HTMLParagraphElement;
-const form = document.querySelector("form") as HTMLFormElement;
 
 form.addEventListener("submit", onSubmit);
+resetBtn.addEventListener("click", onReset);
 
 function onSubmit(e: SubmitEvent): void {
   e.preventDefault();
   resultDesk.classList.remove("empty");
   resultDesk.classList.add("calculated");
+  resultTitle.textContent = "Your results";
+  resultDescription.textContent =
+    "Your results are shown below based on the information you provided. To adjust the results, edit the form and click “calculate repayments” again.";
 
   const form = e.target as HTMLFormElement;
   const amount = (form.elements.namedItem("amount") as HTMLInputElement)
@@ -23,22 +32,35 @@ function onSubmit(e: SubmitEvent): void {
     .valueAsNumber;
   const mortgageType = (
     form.elements.namedItem("mortgageType") as RadioNodeList
-  ).value;
-
-  const P: number = amount;
+  ).value as "repayment" | "interest-only";
+  console.log(rate);
   const r: number = rate / 12 / 100;
-  const n: number = term * 12;
-  const M: number = P * ((r * (1 + r) ** n) / ((1 + r) ** n - 1));
+  console.log(r);
+  const months: number = term * 12;
+  console.log(months);
 
-  monthlyRepayments.textContent = formatCurrency(M);
-  repayOverTerm.textContent = formatCurrency(M * n);
-  //   monthlyRepayments.textContent = `${String(M.toFixed(2))}`;
-  //   repayOverTerm.textContent = `${String((M * n).toFixed(2))}`;
+  if (mortgageType === "repayment") {
+    const M: number =
+      amount * ((r * (1 + r) ** months) / ((1 + r) ** months - 1));
+    console.log(M);
+    monthlyRepayments.textContent = formatCurrency(M);
+    repayOverTerm.textContent = formatCurrency(M * months);
+  }
 
-  console.log("Amount:", P);
-  console.log("r", r);
-  console.log("n:", n);
-  console.log("Mortgage Type:", mortgageType);
+  if (mortgageType === "interest-only") {
+    const M: number = amount * r;
+    monthlyRepayments.textContent = formatCurrency(M);
+    repayOverTerm.textContent = formatCurrency(M * months);
+  }
+}
+
+function onReset(e: MouseEvent) {
+  form.reset();
+  resultDesk.classList.remove("calculated");
+  resultDesk.classList.add("empty");
+  resultTitle.textContent = "Results shown here";
+  resultDescription.textContent =
+    "Complete the form and click “calculate repayments” to see what your monthly repayments would be.";
 }
 
 function formatCurrency(repayment: number): string {
